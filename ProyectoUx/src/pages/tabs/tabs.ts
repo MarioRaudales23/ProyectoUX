ae8eb7e56e83e0e6858274686eef750b70230dff
Luigy08 <luigy9696@gmail.com>
2018-04-08 15:17:28 -0600

    frontend
    .

--- ProyectoUx/src/pages/tabs/tabs.ts
+++ ProyectoUx/src/pages/tabs/tabs.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { InformacionPage } from '../informacion/informacion';
 
 @Component({
-  templateUrl: 'tabs.html'
+  templateUrl: 'tabs.html',
+  
+
 })
+
 export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = InformacionPage;
+
 
   constructor() {
 
