78f97a26cc7c3540328dfcc430aea2bede2cb096
MarioRaudales23 <marioraudales33@unitec.edu>
2018-04-09 19:40:44 -0600

    Login agregado

--- ProyectoUx/src/pages/about/about.ts
+++ ProyectoUx/src/pages/about/about.ts
@@ -1,14 +1,67 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, Input } from '@angular/core';
+import {
+  NavController,
+  AlertController, // To Add Button
+  ActionSheetController, // To delete
+  Button
+} from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from '@firebase/util';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
-
+  currentUser: any;
+  userRef: any;
+  users: AngularFireList<any>;
+  constructor( public navCtrl: NavController,
+               public alertCtrl: AlertController,
+               public actionSheetCtrl: ActionSheetController,
+               public afDatabase: AngularFireDatabase,
+               public afAuth: AngularFireAuth) {
+    this.userRef = afDatabase.list('users');
+    this.users = this.userRef.valueChanges();
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        this.currentUser = null;
+        return;
+      }
+      this.currentUser = { uid: user.uid, photoURL: user.photoURL };
+    });
   }
-
+  show() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'LogOut',
+          role: 'destructive',
+          handler: () => {
+            this.logout();
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((response) => {
+        console.log('resultado login google:', response);
+        const userRef = this.afDatabase.list('users');
+        userRef.update(response.user.uid,
+          {
+            userId: response.user.uid,
+            displayName: response.user.displayName,
+            photoURL: response.user.photoURL
+          });
+      });
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
